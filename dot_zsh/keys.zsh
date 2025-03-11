35b51be769091df8ca58e31c619cd87af9038b98
arielfernandes <0.arielfernandes.0@gmail.com>
2025-03-10 22:34:44 -0300

    config zsh

---
+++ dot_zsh/keys.zsh
@@ -0,0 +1,42 @@
+#bindkey -v
+bindkey '\ew' kill-region
+bindkey -s '\el' "ls\n"
+bindkey "^[[5~" up-line-or-history
+bindkey "^[[6~" down-line-or-history
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+bindkey "^[[H" beginning-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[OH" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[4~" end-of-line
+bindkey "^[OF" end-of-line
+bindkey ' ' magic-space
+bindkey "^F" forward-word
+bindkey "^B" backward-word
+bindkey '^[[Z' reverse-menu-complete
+bindkey '^?' backward-delete-char
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "\e[3~" delete-char
+bindkey '^W' backward-kill-word
+bindkey '^ ' autosuggest-accept
+
+# Use apenas o fzf para ^r
+
+
+# Widgets customizados
+custom-backward-delete-word() {
+    local WORDCHARS=${WORDCHARS/\//}
+    zle backward-delete-word
+}
+zle -N custom-backward-delete-word
+bindkey '^W' custom-backward-delete-word
+
+custom-edit-command-line() {
+    export CUSTOM_EDITOR=nvim
+    zle edit-command-line;
+    unset CUSTOM_EDITOR
+}
+zle -N custom-edit-command-line
+bindkey -M vicmd v custom-edit-command-line
