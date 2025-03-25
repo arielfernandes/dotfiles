7b978c0c0981d4eba95e55ba40175dd02bef20e3
arielfernandes <0.arielfernandes.0@gmail.com>
2025-03-25 17:15:39 -0300

    aliases to git and chezmoi

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -5,6 +5,17 @@ alias mkignore="~/.bin/curl_gitignore.sh "
 alias v='vim'
 
 
+
+#Chezmoi
+alias chz='chezmoi'
+
+# Alias git
+alias g=git
+alias gadd='git add'
+alias gstatus='git status'
+alias gpush='git push'
+
+
 if (( $+commands[nvim] )); then
     alias vim="nvim"
 fi
